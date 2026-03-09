13402938eee05c8035af347532a3e7210916666e
sasideepbillakurthi <sasideepbillakurthi@gmail.com>
2026-03-09 07:32:32 +0530

    Small bug

--- src/config.py
+++ src/config.py
@@ -10,8 +10,8 @@ from pathlib import Path
 # -----------------------------
 DATA_DIR = Path("data")
 
-IMAGE_DIR_TRAIN = DATA_DIR/ "bdd100k_images_100k" / "bdd100k" / "images" / "train"
-IMAGE_DIR_VAL = DATA_DIR / "bdd100k_images_100k" / "bdd100k" / "images" / "val"
+IMAGE_DIR_TRAIN = DATA_DIR/ "bdd100k_images_100k" / "bdd100k" / "images" / "100k" / "train"
+IMAGE_DIR_VAL = DATA_DIR / "bdd100k_images_100k" / "bdd100k" / "images" / "100k" / "val"
 
 LABEL_FILE_TRAIN = DATA_DIR / "bdd100k_labels_release" / "bdd100k" / "labels" / "bdd100k_labels_images_train.json"
 LABEL_FILE_VAL = DATA_DIR / "bdd100k_labels_release" / "bdd100k" / "labels" / "bdd100k_labels_images_val.json"
